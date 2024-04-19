2d2b619d3b46823cc0a917a13feb9fb9c86a45b6
Dissident Genetics <jwelch1123@gmail.com>
2024-04-19 18:34:56 -0400

    Committing MVP, contains debug examples and statements.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Playlistr
 
+Given a message, this program uses the spotify API to assemble a playlist of tracks titles matching the message and saves it to your spotify page. 
+
+## Operation:
+
+First this program uses dotenv to get the ID and Secret provided by spotify. Using the python requests package these are sent to Spotify's API to receive an Access Token which is used to return search results. 
+
+Your message is then passed to several functions which use a sliding and decreasing window to search for matching songs. So we might look for words 1->3, then 1->2, 1 alone, followed by 2->4, 2->3, 2 alone, etc. Whether a matching track is found or not a tuple of the start and end index are returned as a dictionary key along with None or the song information. 
+
+After all combinations have been accessed for a match, the program uses the start and end indexes to create a chain of titles representing your message. For instance we might have found matching tracks for the 1st word, 2nd-3rd, 4th to 6th, etc. These titles need to be joined without missing or repeated words. This can be thought of as a graph network with the nodes representing the junction (or space) between two words in the message, the edges are the track titles which connect the space before the first word in a track and the space after the last word in the track. I implement a recursive search (depth first) to find a path from the first to last nodes (words in the message). If a path isn't found the last word and an error message are displayed.
+
+To add the playlist to a user's profile access is needed via the [PCKE method](https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow). The user is prompted to follow a link and permit access to their username, and ability to edit playlists. They are redirected to a localhost link which is copied and pasted into the command line. 
+
+Finally the user provides a title and description for the playlist which is created via Spotify's API. Tracks are subsequently added along with a playlist cover image and the link is displayed to the user. 
+
+
+## Usage:
+
+I am currently working on a web-app version of this app, in the meantime to run it locally:
+
+1. Follow [this guide from Spotify](https://developer.spotify.com/documentation/web-api/tutorials/getting-started) to register your own app and obtain credentials. 
+2. Download the repo and install the requirements file, `pip install requirements.txt` 
+3. In the directory of `playlistr.py` create a `.env` file and add two lines for the client id and client secret provided by Spotify for your app. 
+    > client_id='abcdedcba'
+    >
+    > client_secret='secret'
+
+4. Program can be invoked from the command line. If it successfully creates the message, a link will be provided to allow access to your spotify account. 
+    - Permissions Used: View username, Create and Edit playlist. 
+5. After allowing permissions the page will redirect to `localhost:8080`, copy the complete URL back into the command line. 
+6. Provide a playlist name and description.
+
+
+## Known Issues:
+- Commonly the tracks found can not be assembled into a phrase which matches yours. This either due to words in your message not being found in the returned tracks or multiple tracks not lining up correctly. Rephrasing your message can help, the program will return the last word it was able to assemble but doesn't differentiate between these two failure modes.
+- Very common short words such as "the", "a", "are" have a low chance of returning as a single word track title. They are usually part of a multi-word track title which should factor into your decision on how to construct your message. 
+- Searching through the spotify API appears to return different track results compared to search via the app. This may lead to tracks not being found by this program but can easily be identified by a search. Increasing the "search_rounds" of the "match_phrase" function may help.
+
+
+## Acknowledgements
+Default photo for playlist by <a href="https://unsplash.com/@namroud?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Namroud Gorguis</a> on <a href="https://unsplash.com/photos/photo-of-black-and-brown-cassette-tape-FZWivbri0Xk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>, edited and downscaled by me
+
+Thanks to [novatorem](https://github.com/novatorem/novatorem/tree/main) for the dotenv usage example and inspiration to pass a playlist image. 
---
+++ playlistr.py
@@ -0,0 +1,301 @@
+import os
+import base64
+import requests
+import secrets
+import hashlib
+import re
+from dotenv import load_dotenv, find_dotenv 
+from nltk.stem import PorterStemmer
+
+load_dotenv(find_dotenv())
+
+
+def generate_code_verifier_and_challenge():
+    code_verifier = secrets.token_urlsafe(64)
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8').replace('=', '')
+    return code_verifier, code_challenge
+
+
+def grantaccess_tokenretreive(client_id):
+    
+    code_verifier, code_challenge = generate_code_verifier_and_challenge()
+
+    redirect_uri = 'http://localhost:8080/'
+    scope = 'playlist-modify-private%20playlist-modify-public%20ugc-image-upload'  # "Space"-separated list of scopes
+
+    auth_url = "https://accounts.spotify.com/authorize?"\
+                + f"client_id={client_id}"\
+                + "&response_type=code"\
+                + f"&redirect_uri={redirect_uri}"\
+                + f"&scope={scope}"\
+                + f"&code_challenge_method=S256&code_challenge={code_challenge}"
+
+    print(f"Please go to this URL and allow access, copy the page that opens:\n{auth_url}")
+
+    authorization_code = input("Paste the url you were redirected to:\n")
+    authorization_code = re.sub("http:\/\/localhost:8080\/\?code=","",authorization_code)
+    
+    
+    token_url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    data = {
+        "client_id": client_id,
+        "grant_type": "authorization_code",
+        "code": authorization_code,
+        "redirect_uri": redirect_uri,
+        "code_verifier": code_verifier,
+    }
+    pkce_token_json = requests.post(token_url, headers=headers, data=data).json()
+    
+    try:
+        pkce_token = pkce_token_json['access_token']
+    except:
+        ValueError("Error: Something went wrong with the token retieval: \n", pkce_token_json)
+
+
+    return pkce_token  # This contains the access token and refresh token
+
+
+def get_search_token(client_id, client_secret):
+    auth_url = 'https://accounts.spotify.com/api/token'
+    auth_headers = {'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode()}
+    auth_data = {'grant_type': 'client_credentials'}
+
+    response = requests.post(auth_url, headers=auth_headers, data=auth_data)
+    
+    if (response.status_code == 200) & (response.json()['access_token'] != None):
+        token = response.json()['access_token']
+        return token
+    else:
+        ValueError("Error: Something went wrong", response.text)
+
+
+def get_text():
+    query = input("Enter song name: ")
+    return query
+
+
+def return_song(response):
+    if response.status_code != 200:
+        print(response.text)
+        raise ValueError(response.status_code)
+    else:
+        return response.json()
+
+    
+def match_phrase(url, headers, query, search_rounds=10, debug=False):
+    response = requests.get(url, headers=headers)
+    returned_songs = return_song(response) 
+    
+
+    stemmer = PorterStemmer()
+    stem_stored = None
+
+    if returned_songs["tracks"]["total"] == 0:
+        print("no matching results for ", query)
+        return None
+
+    i = 0
+    query_token = re.sub("[^a-zA-Z0-9 ]", "", query).strip().lower()
+    query_stem = ' '.join([stemmer.stem(word) for word in query_token.split()])
+    while i <= search_rounds:
+            
+        for track in returned_songs["tracks"]["items"]:
+            track_token = re.sub("[^a-zA-z0-9 ]","", track["name"]).strip().lower()
+            track_stem = ' '.join([stemmer.stem(word) for word in track_token.split()])
+
+            if track_token == query_token:
+                return track
+            elif track_stem == query_stem:
+                stem_stored = track
+            elif i == search_rounds:
+                return None
+
+        try:
+            returned_songs = return_song(requests.get(returned_songs["tracks"]["next"], headers=headers), query)            
+        except:
+            return None
+        
+        i += 1
+    
+    if stem_stored:
+        print("No exact match found. Using stem match")
+        print("Stem match found: ", stem_stored["name"])
+        return stem_stored
+
+
+def search_songs(search_words, token, spotify_search_limit, lookforward, debug=False):
+    '''
+    Ideally this should start with a range of 1 word and expand each pass. (cut off? to prevent full search)
+    '''
+    matching_tracks = {}
+    for i in range(len(search_words)):
+
+        end_idx = i+lookforward if i+lookforward < len(search_words) else len(search_words)
+
+        while end_idx > i:
+            search_phrase = " ".join(search_words[i:end_idx])
+
+            url = "https://api.spotify.com/v1/search?q={"\
+                +search_phrase\
+                +"}&market=US&type=track&limit="\
+                +str(spotify_search_limit)
+            
+            headers = {"Authorization": "Bearer "+token,}
+
+            found_track = match_phrase(url, headers, search_phrase)
+
+            try:
+                matching_tracks[(i,end_idx)] = [search_phrase, \
+                                            found_track["name"], \
+                                            found_track['uri'], \
+                                            found_track["artists"][0]["name"]
+                                        ]
+            except:
+                matching_tracks[(i,end_idx)] = None
+            if debug:
+                print(f"at {i}, {end_idx}",matching_tracks[(i,end_idx)])
+            end_idx -= 1
+    
+    return matching_tracks
+
+
+def recursive_chain(possible_list, end_node, start_node = 0):
+    edges_to_assess = [edge for edge in possible_list if edge[0] == start_node]
+    if edges_to_assess == []:
+        return [("terminated at", start_node)]
+    for edge in edges_to_assess:
+        if edge[1] == end_node:
+            return [edge]
+        next_step = recursive_chain(possible_list, end_node, start_node=edge[1])
+        if next_step[0][0] == "terminated at":
+            return next_step 
+        try:
+            next_step.insert(0,edge)
+            return next_step
+        except:
+            pass
+
+
+def create_playlist(user_id, name, description, token, public=False, collaborative=False):
+    url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"}
+    body = {"name": name, 
+            "description": description, 
+            "public": public, 
+            "collaborative": collaborative
+            }
+    response = requests.post(url, headers=headers, json=body)
+    
+
+    if response.status_code == 201:
+        return response.json()
+    else:
+        raise ValueError(f"Error: Failed to create playlist: {response.text}")
+
+
+def playlist_img(playlist_id, token):
+    endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/images"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "image/jpeg"}
+    # read in a text file from the local dir
+    with open("playlist_img.txt", "rb") as image_file:
+        body = image_file.read()
+
+    response = requests.put(endpoint, headers=headers, data=body)
+    if  response.status_code != 202:
+        raise ValueError(f"Error: Failed to add image to playlist: {response.text}")
+
+
+def add_songs_to_playlist(uri_str_list, playlist_id, bearer_token):
+    endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    headers = {
+        "Authorization": f"Bearer {bearer_token}",
+        "Content-Type": "application/json"}
+    data = {"uris": uri_str_list,
+            "position": 0}
+    response = requests.post(endpoint, headers=headers, json=data)
+
+    try: 
+        response.json()["snapshot_id"]
+        return 
+    except KeyError:
+        raise ValueError(f"Error: Failed to add songs to playlist: {response.text}")
+
+ 
+# a main function to run the program
+def main(query = None, spotify_search_limit=50, lookforward=5):
+    
+    
+        
+    #client_id, client_secret = get_app_credentials().values()
+    client_id = os.getenv("client_id")
+    client_secret = os.getenv("client_secret")
+    token = get_search_token(client_id, client_secret)
+
+    if query is None:
+        query = get_text()
+    search_words = query.split()
+    
+    print("Searching for matching songs...")
+    matching_tracks = search_songs(search_words, token, spotify_search_limit, lookforward)
+
+
+    # Adding for debuging, missing 13, only reaches 10,12
+    Failing_phrase = {(0, 5): None, (0, 4): None, (0, 3): None, (0, 2): None, (0, 1): ['listen', 'Listen', 'spotify:track:2UWiWS7LRNyBZFAOV01WQz', 'Djo'], (1, 6): None, (1, 5): None, (1, 4): None, (1, 3): ['maybe you', 'Maybe You', 'spotify:track:1NIiYcJbjPRorMAw1XknHD', 'hølm'], (1, 2): ['maybe', 'Maybe', 'spotify:track:0xGSeBsG4V8Scc5YqpZQ66', 'Janis Joplin'], (2, 7): None, (2, 
+                        6): None, (2, 5): ['you want to', 'You Want To', 'spotify:track:1hkPzjTARcarv6R7fM5o09', 'Fleeting Joys'], (2, 4): ['you want', 'You Want', 'spotify:track:6R34pagHDtRjcGX4nGzDEQ', 'Dream Sound Masters'], (2, 3): ['you', 'You', 'spotify:track:5Y77SQxEr1eiofPeUTPHxM', 'Lloyd'], (3, 8): None, (3, 7): None, (3, 6): ['want to go', 'Want to Go', 'spotify:track:3uo1bAsNUjVBieIBLiEFQl', 'MELOW MUSIC'], (3, 5): ['want to', 'Want To', 'spotify:track:3kqLybBT5LRSB9QCoR3ojK', 'Sugarland'], (3, 4): ['want', 'Want', 'spotify:track:3dEqPYFBUAOU0fqpudVnJW', 'Olamide'], (4, 9): None, (4, 8): None, (4, 7): None, (4, 6): None, (4, 5): None, 
+                        (5, 10): None, (5, 9): ['go for a walk', 'Go for a Walk', 'spotify:track:3wLg8fgBK1D8gGG0D2aeOn', 'Lissie'], (5, 8): None, (5, 7): None, (5, 6): ['go', 'go', 'spotify:track:4VtRHZ4tBDHaWltVAytlLY', 'Cat Burns'], (6, 11): None, (6, 10): None, (6, 9): ['for a walk', 'For a Walk', 'spotify:track:3r9yRYUFBDn951SK9jeG1g', 'Balance And Composure'], (6, 8): None, (6, 7): None, (7, 12): ['a walk in the park', 'A Walk In The Park', 'spotify:track:16TdDKAsZodsJgP4v4ROeL', 'Laffey'], (7, 11): None, (7, 10): None, (7, 9): ['a walk', 'A Walk', 'spotify:track:0sQNqu37YQQSI3K0ueswyA', 'Tycho'], (7, 8): None, (8, 13): None, (8, 12): ['walk in the park', 'WALK IN THE PARK', 'spotify:track:0XOKietGW4PXK4hs4jyfpO', 'Jack Harlow'], (8, 11): None, (8, 10): ['walk in', 'Walk In', 'spotify:track:1hRwjAMRjUuWmjZ1NG6G1M', 'Rich Amiri'], (8, 9): ['walk', 'walk', 'spotify:track:725UfuWEXWg0C7PCqP8HIz', '310babii'], (9, 13): None, (9, 12): ['in the park', 'In The Park', 'spotify:track:2pvQe4EUSB0Wv6hHtnAIlv', 'TAKARAZI'], (9, 11): None, (9, 10): None, (10, 13): None, (10, 12): ['the park', 'The Park', 'spotify:track:5DTfoiBwW85XSWeUAknhqm', 'Haruomi Hosono'], (10, 11): None, (11, 13): None, (11, 12): ['park', 'Park', 'spotify:track:5hqh0JUxRShhqdaxu7wlz5', 'Isaiah Rashad'], (12, 13): None}
+    # Same phrase, but contains 12,13 to complete the phrase
+    success_path = { (0, 1): ['listen', 'listen!', 'spotify:track:12eAtj918bpsB4Vts8pCMu', 'hako'], (1, 3): ['maybe you', 'Maybe You', 'spotify:track:1NIiYcJbjPRorMAw1XknHD', 'hølm'],  (1, 2): ['maybe', 'Maybe', 'spotify:track:0xGSeBsG4V8Scc5YqpZQ66', 'Janis Joplin'], (2, 5): ['you want to', 'You Want To', 'spotify:track:4nYMwywqyNi2ZCEJB4O5Hh', 'Uptown J Slim'],  
+                    (2, 3): ['you', 'You', 'spotify:track:5Y77SQxEr1eiofPeUTPHxM', 'Lloyd'], (3, 6): ['want to go', 'Want to Go', 'spotify:track:3uo1bAsNUjVBieIBLiEFQl', 'MELOW MUSIC'], (3, 5): ['want to', 'Want To', 'spotify:track:3kqLybBT5LRSB9QCoR3ojK', 'Sugarland'], (3, 4): ['want', 'Want', 'spotify:track:3dEqPYFBUAOU0fqpudVnJW', 'Olamide'],    
+                    (4, 6): ['to go', 'To Go', 'spotify:track:0n5VlW5NVWjpUdkFAlKcHg', 'Home Video Variety Show'], (5, 9): ['go for a walk', 'Go for a Walk', 'spotify:track:52mVUR2Hiy22MUWjuEKuLZ', 'Relaxing Jazz Music'],   (5, 6): ['go', 'go', 'spotify:track:4VtRHZ4tBDHaWltVAytlLY', 'Cat Burns'], (6, 9): ['for a walk', 'For a Walk', 'spotify:track:3r9yRYUFBDn951SK9jeG1g', 'Balance And Composure'],   
+                    (7, 12): ['a walk in the park', 'A Walk In The Park', 'spotify:track:16TdDKAsZodsJgP4v4ROeL', 'Laffey'], (7, 9): ['a walk', 'A Walk', 'spotify:track:0sQNqu37YQQSI3K0ueswyA', 'Tycho'],   (8, 12): ['walk in the park', 'WALK IN THE PARK', 'spotify:track:0XOKietGW4PXK4hs4jyfpO', 'Jack Harlow'], (8, 10): ['walk in', 'Walk In', 'spotify:track:1hRwjAMRjUuWmjZ1NG6G1M', 'Rich Amiri'], 
+                    (8, 9): ['walk', 'walk', 'spotify:track:725UfuWEXWg0C7PCqP8HIz', '310babii'], (9, 12): ['in the park', 'In The Park', 'spotify:track:2pvQe4EUSB0Wv6hHtnAIlv', 'TAKARAZI'],    (10, 12): ['the park', 'The Park', 'spotify:track:5DTfoiBwW85XSWeUAknhqm', 'Haruomi Hosono'], (11, 12): ['park', 'Park', 'spotify:track:5hqh0JUxRShhqdaxu7wlz5', 'Isaiah Rashad'], 
+                    (12, 13): ['sometime?', 'Sometime', 'spotify:track:0k7jQIz3bZBeeCwmBFZzOS', 'Yung Bae']}
+
+
+    ''' Control failing vs working phrase'''
+    track_paths = [key for key in matching_tracks.keys() if matching_tracks[key] is not None]   
+    #track_paths = [key for key in success_path.keys() if success_path[key] is not None]
+    #track_paths = [key for key in Failing_phrase.keys() if Failing_phrase[key] is not None]
+
+    print("Assembling the message from titles...")
+    identified_path = recursive_chain(possible_list=track_paths, end_node=len(search_words))
+    identified_path.sort(key=lambda x: x[0])
+    
+    print(identified_path)
+
+    if identified_path[0][0] == "terminated at":
+        raise ValueError(f"Unable to assemble the phrase. The longest path found ended at index {identified_path[0][1]}, the token not found was: \"{search_words[identified_path[0][1]]}\"")
+
+    
+    pkce_token = grantaccess_tokenretreive(client_id)
+
+    user_info = requests.get("https://api.spotify.com/v1/me", headers={"Authorization": f"Bearer {pkce_token}"})
+    user_id = user_info.json()['id']
+
+    name = input("Enter playlist name: ")
+    description = input("Enter playlist description: ")
+
+    playlist_response = create_playlist(user_id, name, description, pkce_token)
+    playlist_id = playlist_response['id']
+    playlist_url = playlist_response['external_urls']['spotify']
+
+    message_uris = [matching_tracks[edge][2] for edge in identified_path]
+    #message_uris = [success_path[edge][2] for edge in identified_path]
+
+    add_songs_to_playlist(message_uris, playlist_id, pkce_token)
+    playlist_img(playlist_id, pkce_token)
+    print("*"*50)
+    print(f"Playlist created successfully!\n{playlist_url}")
+
+
+if __name__ == "__main__":
+    main(query="listen maybe you want to go for a walk in the park sometime?")
